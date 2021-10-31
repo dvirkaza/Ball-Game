a36a72bd386f315c2134d3ffe8aa1754616ee15f
dvirkaza <85222043+dvirkaza@users.noreply.github.com>
2021-10-31 10:20:53 +0200

    Add files via upload

---
+++ Definitions.java
@@ -0,0 +1,47 @@
+public class Definitions
+{
+    public static final int WINDOW_WIDTH = 700;
+    public static final int WINDOW_HEIGHT = 600;
+    public static final int PLAYER_X = WINDOW_WIDTH/2;
+    public static final int PLAYER_Y = WINDOW_HEIGHT-50;
+    public static final int BALL_POS_X = WINDOW_WIDTH/7;
+    public static final int BALL_POS_Y = WINDOW_HEIGHT/2;
+    public static final int BALL_X_DIR = -1;
+    public static final int BALL_Y_DIR = -1;
+    public static final int PlAYER_LINE_MOVE=20;
+    public static final int WINDOW_X_START=0;
+    public static final int WINDOW_Y_START=0;
+    public static final int START_SCORE=0;
+    public static final int BORDER_THICKNESS=3;
+    public static final int LINE_WIDTH=100;
+    public static final int LINE_HEIGHT=8;
+    public static final int BALL_WIDTH=20;
+    public static final int BALL_HEIGHT=20;
+    public static final int STOP_BALL_X=0;
+    public static final int STOP_BALL_Y=0;
+    public static final int GAME_OVER_X=WINDOW_WIDTH/2-180;
+    public static final int GAME_OVER_Y=WINDOW_HEIGHT/2-20;
+    public static final int GAME_OVER_FONT_SIZE=70;
+    public static final int RESTART_X=(WINDOW_WIDTH/2-180)+70;
+    public static final int RESTART_Y=(WINDOW_HEIGHT/2-20)+70;
+    public static final int RESTART_FONT_SIZE=20;
+    public static final int RESTART_WIDTH=100;
+    public static final int RESTART_HEIGHT=30;
+    public static final int SCORE_RESTART_X=300;
+    public static final int SCORE_RESTART_Y=400;
+    public static final int SCORE_SIZE=25;
+    public static final int SCORE_X=30;
+    public static final int SCORE_Y=70;
+    public static final int WINDOW_X_BORDER_RIGHT=3;
+    public static final int WINDOW_Y_BORDER_START=33;
+    public static final int WINDOW_X_BORDER_LEFT=WINDOW_WIDTH -6;
+    public static final int START_GAME_SPEED=15;
+    public static final int NEXT_LEVEL=5;
+
+
+
+
+
+
+
+}
---
+++ Game.java
@@ -0,0 +1,95 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game extends JPanel  {
+    private Ball ball;
+    private Player player;
+    private static int score;
+    private boolean play;
+    private int gameSpeed;
+
+    public Game() {
+        this.ball = new Ball();
+        this.player=new Player();
+        score=Definitions.START_SCORE;
+        play=true;
+        gameSpeed=Definitions.START_GAME_SPEED;
+
+    }
+
+    public boolean isPlay() {
+        return play;
+    }
+
+    public void setPlay(boolean play) {
+        this.play = play;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public static int getScore() {
+        return score;
+    }
+
+    public static void setScore(int score) {
+        Game.score = score;
+    }
+
+    public int getGameSpeed() {
+        return gameSpeed;
+    }
+
+    public void setGameSpeed(int gameSpeed) {
+        this.gameSpeed = gameSpeed;
+    }
+
+    public void paint(Graphics graphics){
+        ball.paint(graphics);
+        player.paint(graphics);
+    }
+    public void intersects(Rectangle rectangle){
+        if (new Rectangle(this.ball.getBallPosX(),this.ball.getBallPosY(),this.ball.getBallWidth(),this.ball.getBallHeight()).intersects(new Rectangle(this.player.getPlayerX(),
+                this.player.getPlayerY(),this.player.getPlayerWidth(),this.player.getPlayerHeight())) )
+        {
+            this.ball.setBallYDir(-this.ball.getBallYDir());
+            score++;
+            if(score%Definitions.NEXT_LEVEL==0){
+                gameSpeed--;                                   //the speed is faster, and the player is smaller
+                this.player.setPlayerWidth(this.player.getPlayerWidth()-1);
+            }
+
+        }
+
+
+    }
+    public boolean finish(){
+        if (this.ball.getBallPosY()>Definitions.WINDOW_HEIGHT)
+        {
+            this.ball.setBallPosY(Definitions.STOP_BALL_Y);
+            this.ball.setBallPosX(Definitions.STOP_BALL_X);
+            return true;
+
+        }
+        return false;
+
+    }
+
+
+}
+
